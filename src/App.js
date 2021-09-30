1f1680ced0a4158216bed7c37323bbf6f90e1e6c
Imran Rahman <mrimranoor@gmail.com>
2021-09-30 12:14:47 +0100

    Login and register page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import TopBar from "./components/topbar/TopBar";
-import Settings from "./components/pages/settings/Settings";
+import Register from "./components/pages/register/Register";
 
 function App() {
   return (
     <>
       <TopBar />
-      <Settings />
+      <Register />
     </>
   );
 }
